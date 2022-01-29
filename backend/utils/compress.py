991312a5e8cf01b702f85f3fc2dccd8dfab377e8
sachin <sachinsom507@gmail.com>
2022-01-29 12:05:38 +0530

    Added Compression and decompression functions.

---
+++ backend/utils/compress.py
@@ -0,0 +1,22 @@
+import zlib, sys, bz2
+
+def compresss_file_content(data):
+    """
+    Compresses the given data and returns
+    compressed data.
+    """
+    print("Before Compression: ", sys.getsizeof(data))
+    compressed_data =  bz2.compress(data, zlib.Z_BEST_COMPRESSION)
+    print("After Compression: ", sys.getsizeof(compressed_data))
+    return compressed_data
+
+
+def decompress_file_content(data):
+    """
+    Decompresss the given data file and returns
+    decompressed data.
+    """
+    print("Before Decompression: ", sys.getsizeof(data))
+    decompressed_data = zlib.decompress(data)
+    print("After Decompression: ", sys.getsizeof(decompressed_data))
+    return decompressed_data
--- backend/utils/io_helper.py
+++ backend/utils/io_helper.py
@@ -1,11 +1,13 @@
+from backend.utils.compress import compresss_file_content
 from backend.utils.crypto import encrypt_file
 
 
 def write_to_file(filename, content, key):
     try:
         with open(filename, 'wb') as f:
-            encrypted_data = encrypt_file(content, key)
-            f.write(encrypted_data)
+            encrypted_data = encrypt_file(content, key) # Encryption
+            compressed_data = compresss_file_content(encrypted_data) # Compression
+            f.write(compressed_data) # write
             f.close()
     except Exception as e:
         print(e)
