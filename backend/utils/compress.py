5e1759c2892afcadeddf7d4f01c9993284566ebd
sachin <sachinsom507@gmail.com>
2022-01-30 10:20:10 +0530

    Code Refactor.

--- backend/utils/compress.py
+++ backend/utils/compress.py
@@ -5,9 +5,9 @@ def compresss_file_content(data):
     Compresses the given data and returns
     compressed data.
     """
-    print("Before Compression: ", sys.getsizeof(data))
+    print("Encrypted File Size: ", sys.getsizeof(data))
     compressed_data =  zlib.compress(data, zlib.Z_BEST_COMPRESSION)
-    print("After Compression: ", sys.getsizeof(compressed_data))
+    print("Encrypted + Compressed File Size: ", sys.getsizeof(compressed_data))
     return compressed_data
 
 
--- backend/utils/io_helper.py
+++ backend/utils/io_helper.py
@@ -1,10 +1,15 @@
+import sys
+from fastapi import HTTPException
+
 from backend.utils.compress import compresss_file_content, decompress_file_content
 from backend.utils.crypto import decrypt_file, encrypt_file
+
 
 
 def write_to_file(filename, content, key):
     try:
         with open(filename, 'wb') as f:
+            print("Normal File Size: ", sys.getsizeof(content))
             encrypted_data = encrypt_file(content, key) # Encryption
             compressed_data = compresss_file_content(encrypted_data) # Compression
             f.write(compressed_data) # write
@@ -14,15 +19,16 @@ def write_to_file(filename, content, key):
         return False
     return True
 
+
+
 def read_from_file(filename, key):
     try:
         with open(filename, 'rb') as f:
             data = f.read()
-            print(type(data))
             decompressed_data = decompress_file_content(data) # Decompression
             decrypted_data = decrypt_file(decompressed_data, key) # Decryption
             f.close()
     except Exception as e:
         print(e)
-        return False
+        raise HTTPException(status_code=500, detail=e)
     return decrypted_data
