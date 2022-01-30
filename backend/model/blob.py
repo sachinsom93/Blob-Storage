47defa3e1961a414c655b5483d3cb5352f8d6c8b
sachin <sachinsom507@gmail.com>
2022-01-30 09:43:56 +0530

    Added New Fields in Blob Model Class.

--- backend/model/blob.py
+++ backend/model/blob.py
@@ -15,15 +15,19 @@ class Blob(Base):
 
     # Property Declaration
     id = Column(Integer, primary_key=True, autoincrement=True)
+    blob_name = Column(String, nullable=False)
+    content_type = Column(String, nullable=False)
     blob_url = Column(String, nullable=False, unique=True)
     user_id = Column(Integer, ForeignKey("User.id"))
     user = relationship("User", back_populates="blob")
 
     # Initialize Token
-    def __init__(self, blob_url, user):
+    def __init__(self, blob_url, blob_name, content_type, user):
         """
         Constructor for BlackListedToken Model Class.
         """
         self.blob_url = blob_url
+        self.blob_name = blob_name
+        self.content_type = content_type
         self.user = user
 
